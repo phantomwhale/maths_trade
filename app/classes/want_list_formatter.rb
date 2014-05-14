4832261cb2d9f02fb94a54d08efb7b03f35c6aeb
Ben Turner <ben.turner@pobox.com>
2014-05-14 16:09:29 +1000

    Show cash offers on want list

--- app/classes/want_list_formatter.rb
+++ app/classes/want_list_formatter.rb
@@ -1,18 +1,29 @@
 class WantListFormatter
 
-  def initialize(username, offers)
+  def initialize(username, offers, wants)
     @username = username
     @offers = offers
+    @wants = wants
   end
 
   def formatted_trades
     @offers.map do |offer|
       trade_codes = offer.trades.collect(&:trade_code).join(" ") if offer.trades
-      "(#{@username}) #{offer.trade_code}: #{trade_codes}"
+      format_line(offer.trade_code, trade_codes)
+    end + 
+    @wants.map do |want|
+      trade_code = want.trade_code
+      format_line("$#{want.cash_offer}", trade_code)
     end
   end
 
   def trades
     formatted_trades.join("\n")
+  end
+
+  private 
+
+  def format_line(offer_code, trade_codes)
+    "(#{@username}) #{offer_code}: #{trade_codes}"
   end
 end
--- app/controllers/want_lists_controller.rb
+++ app/controllers/want_lists_controller.rb
@@ -1,6 +1,7 @@
 class WantListsController < ApplicationController
   def show
     offers = Offer.find_by_username(current_user.username)
-    @formatter = WantListFormatter.new(current_user.username, offers)
+    wants = Want.where(:user => current_user).where('cash_offer > 0').order('list_entry_id')
+    @formatter = WantListFormatter.new(current_user.username, offers, wants)
   end
 end
