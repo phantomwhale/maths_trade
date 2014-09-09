c3dc6e3d280cc64f241565d5a2106f93f4183f4c
Ben Turner <ben.turner@pobox.com>
2014-09-09 20:12:47 +1000

    Fixed sortable jquery asset naming

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery.ui.sortable
+//= require jquery-ui/sortable
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
